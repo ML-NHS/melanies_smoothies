0d00df637304be9b75570d9d48dfc43d17b2b915
ML-NHS <155739896+ML-NHS@users.noreply.github.com>
2026-02-10 14:27:27 +0000

    Update streamlit_app.py
    wrapped 'stretch' in quotes to correct syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        st_df = st.dataframe(data=smoothiefroot_response.json(), width=stretch)
+        st_df = st.dataframe(data=smoothiefroot_response.json(), width='stretch')
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
